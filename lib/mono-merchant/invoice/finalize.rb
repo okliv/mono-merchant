3e6c1b6a4ef1160f82a5f8e556110abeaadafd6a
ok <ok@entitative.com>
2024-09-30 13:41:02 +0300

    Finalize without explicit amount

--- lib/mono-merchant/invoice/finalize.rb
+++ lib/mono-merchant/invoice/finalize.rb
@@ -7,7 +7,7 @@ module MonoMerchant
     class Finalize < ApiRequest
       attr_reader :invoice_id, :amount, :items
 
-      def initialize(invoice_id, amount=nil, items: [])
+      def initialize(invoice_id, amount = nil, items: [])
         @invoice_id = invoice_id
         @amount = amount
         @items = items.map { |i| Item.new(i).to_hash }
@@ -16,9 +16,29 @@ module MonoMerchant
 
       protected
 
+      def implicit_amount
+        resp = Status.new(invoice_id).send(:response)
+        err_hsh = {}
+        if resp.is_a?(Hash)
+          %w[errCode failureReason].each do |err|
+            err_msg = resp[err]
+            next unless err_msg
+            err_hsh[err] = err_msg
+          end
+        else
+          err_hsh['wrongData'] = 'response is not a Hash'
+        end
+        unless err_hsh.empty?
+          warn err_hsh
+          return nil
+        end
+
+        resp['amount'] if resp['status'] == 'hold'
+      end
+
       def body
         { invoiceId: invoice_id,
-          amount: amount,
+          amount: amount || implicit_amount,
           items: items.presence
         }.compact!
       end
